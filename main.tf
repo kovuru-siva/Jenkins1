799b65a4e523e816a0ed8edd85bd7b5536b40f35
siva K <reddysivak97@gmail.com>
2024-10-15 13:21:21 +0000

    adding files

--- main.tf
+++ main.tf
@@ -1,27 +1,27 @@
 provider "aws" {
-    region = "ap-south-1"
-  
+  region = "ap-south-1"
+
 }
 
 module "vpc" {
-    source = "./Module/vpc"
-  
+  source = "./Module/vpc"
+
 }
 
 
 resource "aws_instance" "web" {
-    ami = var.instance_ami
-    instance_type = lookup(var.instance-type, ap-south-1, t2-medium )
-    subnet_id = [module.vpc.publicsubnet]
-    security_groups = [module.vpc.security-group]
-    key_name = aws_key_pair.webkey
-  
+  ami             = var.instance_ami
+  instance_type   = lookup(var.instance-type, "ap-south-1", "t2.medium")
+  subnet_id       = module.vpc.publicsubnet
+  security_groups = [module.vpc.security-group]
+  key_name        = aws_key_pair.webkey.key_name
+
 }
 
 resource "aws_key_pair" "webkey" {
-    key_name = "webkey"
-    public_key = file(var.public-key)
-  
+  key_name   = "webkey"
+  public_key = file(var.public-key)
+
 }
 
 
--- variable.tf
+++ variable.tf
@@ -1,17 +1,17 @@
 variable "instance_ami" {
-    default = "ami-02b49a24cfb9541c"
-  
+  default = "ami-078264b8ba71bc45e"
+
 }
 
 variable "instance-type" {
-    type = map(string)
-    default = {
-      "ap-south-1" = "t2-medium"
-    }
-  
+  type = map(string)
+  default = {
+    "ap-south-1" = "t2-medium"
+  }
+
 }
 
 variable "public-key" {
-    default = "~/.ssh/test.pub"
-  
+  default = "~/.ssh/id_ed25519.pub"
+
 }
